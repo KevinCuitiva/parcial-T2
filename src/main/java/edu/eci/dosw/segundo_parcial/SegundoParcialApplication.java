9a26c7d4ec383c12ae92ff33649325fd0f788c1a
KevinCuitiva <kevin.cuitiva-p@mail.escuelaing.edu.co>
2026-04-08 15:28:06 -0500

    first configuration

---
+++ src/main/java/edu/eci/dosw/segundo_parcial/SegundoParcialApplication.java
@@ -0,0 +1,16 @@
+package edu.eci.dosw.segundo_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Punto de entrada de la aplicacion.
+ *
+ */
+@SpringBootApplication
+public class SegundoParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegundoParcialApplication.class, args);
+    }
+}
